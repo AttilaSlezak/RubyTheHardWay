4f0e070950e4402b1db172e758c49e1149f238b2
Slezak Attila <attila.slezak@gmail.com>
2016-05-16 17:53:17 +0100

    The first 8 exercises have been added

---
+++ ex1.rb
@@ -0,0 +1,8 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts 'Yay! Printin.'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+# puts "This won't be printed because of the octothorpe in the beginning of the line."
