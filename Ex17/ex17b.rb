91c80c1b56ea3793e542a2f4369989bcd5f520f3
Slezak Attila <attila.slezak@gmail.com>
2016-05-22 16:08:37 +0100

    First 25 exercises are ready

---
+++ Ex17/ex17b.rb
@@ -0,0 +1,17 @@
+﻿from_file, to_file = ARGV
+script = $0
+
+puts "Copying from #{from_file} to #{to_file}"
+
+File.open(to_file, 'w') do |fw|
+  File.open(from_file) do |f| 
+    puts "The output file is #{f.read().length} bytes long"
+    puts "Does the output file exist? #{File.exists? to_file}"
+    puts "Ready, hit RETURN to continue, CTRL-C to abort."
+    STDIN.gets
+    f.seek(0)
+    fw.write(f.read())
+  end
+end
+
+puts "Alright, all done."
---
+++ Ex19/ex19b.rb
@@ -0,0 +1,28 @@
+﻿def my_function(arg)
+  puts arg
+  if arg.is_a? Array
+    puts "It's an array. It has #{arg.size} elements."
+  elsif arg.is_a? Integer
+	puts "It's an integer."
+	puts arg > 100 ? "And it's bigger than 100." : "And it's smaller than 101."
+  elsif arg.is_a? String
+    puts "It's a string. And it contents #{arg.length} characters."
+  elsif arg.is_a? Float
+    puts "It's a float."
+  end
+end
+
+my_function([12, "Empty", 23])
+my_function(23)
+my_function(102)
+my_function(4.5)
+my_function(12.0)
+my_function("My text.")
+
+longer_text = <<LONGER_TEXT
+This is a longer text here.
+You should know it.
+But now I finish it.
+LONGER_TEXT
+
+my_function(longer_text)
