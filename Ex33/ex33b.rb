b307bd63930702381f21bd502f2b6f144b90aaca
Slezak Attila <attila.slezak@gmail.com>
2016-06-05 18:29:34 +0100

    First 35 exercises are ready

---
+++ Ex33/ex33b.rb
@@ -0,0 +1,20 @@
+﻿def while_function(num, max_num)
+  numbers = []
+  while num < max_num
+    puts "At the top num is #{num}"
+    numbers.push(num)
+  
+    num += 8
+    puts "Numbers now: #{numbers}"
+    puts "At the bottom num is #{num}"
+  end
+  numbers
+end
+
+numbers = while_function(3, 28)
+
+puts "The numbers: "
+
+for num in numbers
+  puts num
+end
